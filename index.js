55d5e3161a3e6ecebcbe130bbce8c1a846e698b1
Himanshu Kataria <katariahk03@gmail.com>
2022-08-13 17:24:46 +0530

    Init Project, HTML, CSS, JS add

---
+++ index.js
@@ -0,0 +1,12 @@
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+const button = document.getElementById("get-quote");
+async function fetchQuote() {
+  const response = await fetch("https://api.quotable.io/random");
+  const data = await response.json();
+  quote.innerText = data.content;
+  author.innerText = data.author;
+  
+}
+fetchQuote();
+button.addEventListener("click", () => fetchQuote());
